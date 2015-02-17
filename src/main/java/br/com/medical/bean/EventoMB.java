498c84cae5e456956d79d985fc250e02d50a01ab
RicardoBorgesGO <ricardoborges.ufg@gmail.com>
2015-02-17 02:59:31 -0200

    Modificações do client de eventos

--- src/main/java/br/com/medical/bean/EventoMB.java
+++ src/main/java/br/com/medical/bean/EventoMB.java
@@ -1,6 +1,8 @@
 package br.com.medical.bean;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
@@ -8,6 +10,7 @@ import javax.faces.bean.ViewScoped;
 
 import com.upschool.entity.Evento;
 import com.upschool.util.UtilConverter;
+import com.upschool.util.UtilJson;
 
 @ManagedBean
 @ViewScoped
@@ -19,15 +22,22 @@ public class EventoMB implements Serializable {
 	private static final long serialVersionUID = -9148064236378193923L;
 	
 	private Evento evento;
+	
+	@SuppressWarnings("rawtypes")
+	private List<? extends ArrayList> eventos;
 
 	@PostConstruct
 	public void init() {
 		evento = new Evento();
+		//TODO substituir, e pegar os elementos por Jquery
+//		eventos = UtilJson.getAllObjectJson("http://localhost:8080/spring-jpa/rest/evento/getEventos", new ArrayList<Evento>().getClass());
 	}
 
 	public String adicionarEvento() {
 		String eventJson = UtilConverter.objectToJson(evento);
-		System.out.println(eventJson);
+		
+		//TODO Colocar url em um arquivo ou classe de configuracao
+		UtilJson.postJson("http://localhost:8080/spring-jpa/rest/evento/setEvento", eventJson);
 
 		return "";
 	}
