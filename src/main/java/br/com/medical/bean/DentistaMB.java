ca09400f5aa7320975712a5b2c70f8f78ae36ca6
RicardoBorgesGO <ricardoborges.ufg@gmail.com>
2015-02-19 16:56:40 -0200

    Continuação do CRUD de dentista e consulta em webservice de CEP.

--- src/main/java/br/com/medical/bean/DentistaMB.java
+++ src/main/java/br/com/medical/bean/DentistaMB.java
@@ -6,7 +6,12 @@ import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
+import br.com.commons.constant.EnumEstadoCivil;
+import br.com.commons.constant.EnumSexo;
+import br.com.commons.constant.EnumTipoTelefone;
+
 import com.upschool.entity.Dentista;
+import com.upschool.entity.Telefone;
 
 @ViewScoped
 @ManagedBean
@@ -19,9 +24,17 @@ public class DentistaMB implements Serializable {
 
 	private Dentista dentista;
 
+	private Telefone telefoneComercial;
+	private Telefone telefoneCelular;
+	private Telefone telefoneResidencial;
+
 	@PostConstruct
 	public void init() {
 		dentista = new Dentista();
+
+		telefoneCelular = new Telefone(EnumTipoTelefone.CELULAR);
+		telefoneComercial = new Telefone(EnumTipoTelefone.COMERCIAL);
+		telefoneResidencial = new Telefone(EnumTipoTelefone.RESIDENCIAL);
 	}
 
 	public Dentista getDentista() {
@@ -30,6 +43,47 @@ public class DentistaMB implements Serializable {
 
 	public void setDentista(Dentista dentista) {
 		this.dentista = dentista;
+	}
+
+	public Telefone getTelefoneComercial() {
+		return telefoneComercial;
+	}
+
+	public void setTelefoneComercial(Telefone telefoneComercial) {
+		this.telefoneComercial = telefoneComercial;
+	}
+
+	public Telefone getTelefoneCelular() {
+		return telefoneCelular;
+	}
+
+	public void setTelefoneCelular(Telefone telefoneCelular) {
+		this.telefoneCelular = telefoneCelular;
+	}
+
+	public Telefone getTelefoneResidencial() {
+		return telefoneResidencial;
+	}
+
+	public void setTelefoneResidencial(Telefone telefoneResidencial) {
+		this.telefoneResidencial = telefoneResidencial;
+	}
+
+	public EnumSexo[] getSexos() {
+		return EnumSexo.values();
+	}
+
+	public EnumEstadoCivil[] getEstadosCivis() {
+		return EnumEstadoCivil.values();
+	}
+
+	public String salvar() {
+		if (telefoneCelular.getNumeroDoTelefone() != null && !telefoneCelular.getNumeroDoTelefone().isEmpty()) dentista.adicionaTelefone(telefoneCelular);
+		if (telefoneComercial.getNumeroDoTelefone() != null && telefoneComercial.getNumeroDoTelefone().isEmpty()) dentista.adicionaTelefone(telefoneComercial);
+		if (telefoneResidencial.getNumeroDoTelefone() != null && telefoneResidencial.getNumeroDoTelefone().isEmpty()) dentista.adicionaTelefone(telefoneResidencial);
+		
+		System.out.println(dentista);
+		return "";
 	}
 
 }
