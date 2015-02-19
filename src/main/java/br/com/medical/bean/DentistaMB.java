e580bf72e215482f885ea056206f2b9efa938607
Ricardo Borges <ricardoborges.ufg@gmail.com>
2015-02-19 00:56:31 -0200

    Continuação da classe dentistaMB.

--- src/main/java/br/com/medical/bean/DentistaMB.java
+++ src/main/java/br/com/medical/bean/DentistaMB.java
@@ -2,8 +2,11 @@ package br.com.medical.bean;
 
 import java.io.Serializable;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+
+import com.upschool.entity.Dentista;
 
 @ViewScoped
 @ManagedBean
@@ -13,7 +16,20 @@ public class DentistaMB implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = -3763643560114401069L;
-	
-	
+
+	private Dentista dentista;
+
+	@PostConstruct
+	public void init() {
+		dentista = new Dentista();
+	}
+
+	public Dentista getDentista() {
+		return dentista;
+	}
+
+	public void setDentista(Dentista dentista) {
+		this.dentista = dentista;
+	}
 
 }
