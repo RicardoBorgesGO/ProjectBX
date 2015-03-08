e2af051270d659f6d491dcd938535e98d73953b0
RicardoBorgesGO <ricardoborges.ufg@gmail.com>
2015-03-08 18:45:46 -0300

    Inclusão do autocomplete e manipulação da logo.

--- src/main/webapp/resources/js/autocomplete.js
+++ src/main/webapp/resources/js/autocomplete.js
@@ -1,51 +1,125 @@
-/**
- * 
- */
-var substringMatcher = function(strs) {
-	return function findMatches(q, cb) {
-		var matches, substrRegex;
-
-		// an array that will be populated with substring matches
-		matches = [];
-
-		// regex used to determine if a string contains the substring `q`
-		substrRegex = new RegExp(q, 'i');
-
-		// iterate through the pool of strings and for any string that
-		// contains the substring `q`, add it to the `matches` array
-		$.each(strs, function(i, str) {
-			if (substrRegex.test(str)) {
-				// the typeahead jQuery plugin expects suggestions to a
-				// JavaScript object, refer to typeahead docs for more info
-				matches.push({
-					value : str
-				});
-			}
-		});
-
-		cb(matches);
-	};
-};
-
-var pacientes = [];
-
-$.ajax({
-    type: "GET",
-    url: 'http://localhost:8080/spring-jpa/rest/paciente/getPacientes',
-	dataType: "json",
-	success: function(data){
-		for (var i = 0; i < data.length; i++) {
-			pacientes.push(data[i].nome);
-		}
-	}
-});
-
+//PACIENTE
 $('.form-group .search-paciente').typeahead({
 	hint : true,
 	highlight : true,
-	minLength : 1
+	minLength : 1,
 }, {
-	name : 'pacientes',
-	displayKey : 'value',
-	source : substringMatcher(pacientes)
+    source: function(query, process) {
+        var $url = 'http://localhost:8080/spring-jpa/rest/paciente/getPacientes';
+        var $items = new Array;
+        $items = [];
+        $.ajax({
+            url: $url,
+            dataType: "json",
+            type: "GET",
+            success: function(data) {
+                $.map(data, function(data){
+                    var group;
+                    group = {
+                        id: data.id,
+                        name: data.nome,
+                        toString: function () {
+                            return JSON.stringify(this);
+                            //return this.app;
+                        },
+                        toLowerCase: function () {
+                            return this.name.toLowerCase();
+                        },
+                        indexOf: function (string) {
+                            return String.prototype.indexOf.apply(this.name, arguments);
+                        },
+                        replace: function (string) {
+                            var value = '';
+                            value +=  this.name;
+                            if(typeof(this.level) != 'undefined') {
+                                value += ' <span class="pull-right muted">';
+                                value += this.level;
+                                value += '</span>';
+                            }
+                            return String.prototype.replace.apply('<div style="padding: 10px; font-size: 1.5em;">' + value + '</div>', arguments);
+                        }
+                    };
+                    
+                    var substrRegex;
+                    substrRegex = new RegExp(query, 'i');
+                    
+                    if (substrRegex.test(group.name)) {
+                    	$items.push(group);
+        			}
+                });
+                process($items);
+            }
+        });
+    },
+    name : 'pacientes',
+	displayKey : 'name',
+    items: 10,
+    minLength: 2
+});
+
+$('.form-group .search-paciente').on('typeahead:selected', function(evt, item) {
+	$('#pacienteID').val(item.id);
+});
+
+//COLABORADOR
+$('.form-group .search-colaborador').typeahead({
+	hint : true,
+	highlight : true,
+	minLength : 1,
+}, {
+    source: function(query, process) {
+        var $url = 'http://localhost:8080/spring-jpa/rest/colaborador/getColaboradores';
+        var $items = new Array;
+        $items = [];
+        $.ajax({
+            url: $url,
+            dataType: "json",
+            type: "GET",
+            success: function(data) {
+                $.map(data, function(data){
+                    var group;
+                    group = {
+                        id: data.id,
+                        name: data.nome,
+                        toString: function () {
+                            return JSON.stringify(this);
+                            //return this.app;
+                        },
+                        toLowerCase: function () {
+                            return this.name.toLowerCase();
+                        },
+                        indexOf: function (string) {
+                            return String.prototype.indexOf.apply(this.name, arguments);
+                        },
+                        replace: function (string) {
+                            var value = '';
+                            value +=  this.name;
+                            if(typeof(this.level) != 'undefined') {
+                                value += ' <span class="pull-right muted">';
+                                value += this.level;
+                                value += '</span>';
+                            }
+                            return String.prototype.replace.apply('<div style="padding: 10px; font-size: 1.5em;">' + value + '</div>', arguments);
+                        }
+                    };
+                    
+                    var substrRegex;
+                    substrRegex = new RegExp(query, 'i');
+                    
+                    if (substrRegex.test(group.name)) {
+                    	$items.push(group);
+        			}
+                });
+                process($items);
+            }
+        });
+    },
+    name : 'colaboradores',
+	displayKey : 'name',
+    items: 10,
+    minLength: 2
+});
+
+$('.form-group .search-colaborador').on('typeahead:selected', function(evt, item) {
+	$('#colaboradorID').val(item.id);
 });
